27fa87dd357a5d8e048423dfee9a42bb80a6272e
Adriel <addrriel@hotmail.com>
2025-09-28 08:49:55 -0500

    Exposición Angular 20

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { About } from './pages/about/about';
+import { Contact } from './pages/contact/contact';
+import { Products } from './pages/products/products';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path: '', component: Home},
+    {path: 'nosotros', component: About},
+    {path: 'contacto', component: Contact},
+    {path: 'productos', component: Products},
+    {path: '**', redirectTo: ''}
+
+
+];
